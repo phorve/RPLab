072da834d780534febe05ce09ed2daa8529a2b05
Patrick Horve <pfh@uoregon.edu>
2021-11-03 11:38:34 -0700

    remove coloring (not working) and add more detail to the progress summaries

--- Imaris_prep.sh
+++ Imaris_prep.sh
@@ -7,15 +7,15 @@ mkdir ./Imaris_temp
 mkdir ./Imaris_to_convert
 for i in $(seq 1 $times)
 do
-  echo -e "\e[44mThis is timepoint ""$i"
+  echo "This is timepoint ""$i"
   mv ./Fish1/Timepoint"$i"/Pos1/zStack/GFP/Default/*.tif ./Imaris_temp
   cd ./Imaris_temp
   slices=$(ls -1q img* | wc -l)
   cd ../
-  echo -e "\e[31mThere are""$slices"" slices."
+  echo "There are""$slices"" slices."
   for f in $(seq -f "%03g" 0 $slices)
   do
-    echo -e "\e[92mThis is slice ""$f"
+    echo "This is slice ""$f"" from timepoint ""$i"
     mv ./Imaris_temp/img_channel000_position000_time000000000_z"$f".tif ./Imaris_temp/img_channel000_position000_time"$i"_z"$f".tif
     mv ./Imaris_temp/img_channel000_position000_time"$i"_z"$f".tif ./Imaris_to_convert
     echo "Finished moving and renaming slice #""$f"
