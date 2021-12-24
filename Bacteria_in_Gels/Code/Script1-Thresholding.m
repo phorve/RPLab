13efd001182598252ea917547afab3b2c80a7edc
Patrick Horve <pfh@uoregon.edu>
2021-12-24 09:00:29 -0800

    Update Script1-Thresholding.m

--- Bacteria_in_Gels/Code/Script1-Thresholding.m
+++ Bacteria_in_Gels/Code/Script1-Thresholding.m
@@ -1,6 +1,6 @@
 % This script reads in multiple .tif files output from a
 % custom lightsheet flourescence microscope and outputs a table of the
-% calculated thresholds from each time point 
+% calculated thresholds from each time point
 
 %=========================================================================%
 % Patrick Horve + Raghu Parthasarathy - Fall 2021
@@ -19,21 +19,21 @@ startpath = string(userpath);
 cd (startpath); %path to the folder that holds all the .tif stacks
 
 % Count the number of timepoints that are present in this image acquisition
-% go to our folder with all the timepoints 
+% go to our folder with all the timepoints
 fish='/Fish1';
 cd './Fish1'
-% Counts the files in that folder 
+% Counts the files in that folder
 all_files = dir;
 all_dir = all_files([all_files(:).isdir]);
 timepoints = numel(all_dir)-2;
 
 % Set the thresholds for this dataset
-minPixels = 50; % minimum number of pixels to include after morphological closing 
+minPixels = 50; % minimum number of pixels to include after morphological closing
 pixels=num2str(minPixels);
 
 % Start looping through the timepoints
 output = table;
-for t = 1:timepoints 
+for t = 1:timepoints
     disp("This is timepoint #"+t); % track the progress of the script
     time=string(t); % make the timepoint something that we can use in a path
     fileFolder = strcat(startpath,fish,"/Timepoint",time,"/Pos1/zStack/GFP/Default"); % where are all of our .tif files located?
@@ -52,11 +52,11 @@ for t = 1:timepoints
         stack(:,:,i) = img; % add this image to the image stack
         disp(string(i*100.0/D) + "% of the images from this timepoint added to the 3D array"); % for seeing the reading progress
     end
-    % Cropping (manual) - only use this if you want to crop out part of the image 
+    % Cropping (manual) - only use this if you want to crop out part of the image
 %     fprintf('Original x, y, z sizes: %d (x), %d (y), %d(z).\n', ...
 %         size(stack,2), size(stack,1), size(stack,3));
 %     % Manually set the region to keep; enter the numbers here under xmin, ymin, and zmin arguments.
-%     xmin = 450; 
+%     xmin = 450;
 %     xmax = size(stack,2);
 %     ymin = 1;
 %     ymax = size(stack,1);
@@ -65,7 +65,7 @@ for t = 1:timepoints
 %     stack = stack(ymin:ymax, xmin:xmax, zmin:zmax);
 %     fprintf('New x, y, z sizes: %d (x), %d (y), %d(z).\n', ...
 %         size(stack,2), size(stack,1), size(stack,3));
-    % Analysis for the current timepoint 
+    % Analysis for the current timepoint
     % Calculate statistics on a subset of pixels, faster than using the wholearray.
     disp("Determining the level to apply to the full stack");
     Npixels = numel(stack); % total number of pixels
@@ -78,8 +78,8 @@ for t = 1:timepoints
 % Threshold (z)
     z = 4; % threshold = median + z standard deviations
     level = meanIntensity + z*stdevIntensity;  % threshold level, not in [0,1]
-    
-% Save the thresholds for each timepoint 
+
+% Save the thresholds for each timepoint
 cd ../
 if t==1
     output.timepoint = t;
@@ -97,6 +97,6 @@ else
     output = [output ; tmp_table];
 end
 end
-% Save our threshold values to a table to access later on 
+% Save our threshold values to a table to access later on
 cd (startpath); %path to the folder that holds all the .tif stacks
 writetable(output, "Timeseries-Intensities.txt")
